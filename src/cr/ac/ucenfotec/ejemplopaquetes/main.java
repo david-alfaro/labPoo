5a55f4677f416a5024d6a2ab5423467bc438d925
david-alfaro-cenfotec-proyecto1 <65271642+david-alfaro-cenfotec-proyecto1@users.noreply.github.com>
2020-09-23 21:34:17 -0600

    segunda modificacion

--- src/cr/ac/ucenfotec/ejemplopaquetes/main.java
+++ src/cr/ac/ucenfotec/ejemplopaquetes/main.java
@@ -6,7 +6,7 @@ public class main {
 
     
     public static void main(String[] args) {
-        
+        System.out.println("Hola mundo, es una prueba para control de version en netbeans");
     }
     
 }
